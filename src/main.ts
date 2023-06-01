119813019d715f943176150e446cea4cfbf81890
Rafael França <rafaelfranca@live.com>
2023-06-01 01:03:15 -0300

    Task [00:01] remove styles

--- src/main.ts
+++ src/main.ts
@@ -9,7 +9,7 @@ import router from './router'
 import { createVuetify } from 'vuetify'
 import * as components from 'vuetify/components'
 import * as directives from 'vuetify/directives'
-// import 'vuetify/styles'
+import 'vuetify/styles'
 
 const vuetify = createVuetify({
   components,
