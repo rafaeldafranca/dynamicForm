8ef9dc2473ccf385ae5f9ca5cee54b5fe4b6f9cc
Rafael França <rafaelfranca@live.com>
2023-06-01 00:49:06 -0300

    Task [00:01] vuetify styles

--- src/main.ts
+++ src/main.ts
@@ -6,10 +6,10 @@ import router from './router'
 
 
 // Vuetify
-import 'vuetify/styles'
 import { createVuetify } from 'vuetify'
 import * as components from 'vuetify/components'
 import * as directives from 'vuetify/directives'
+import 'vuetify/styles'
 
 const vuetify = createVuetify({
   components,
